4e46c9175eadc711a85467578b2e2d9bc654f758
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-10-31 10:47:27 -0300

    feat: create get by id

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -12,7 +12,7 @@ usuariosRoutes.get("/", (req, res) => {
         message:
         usuarios.length == 0
         ? "Não há usuários cadastrados"
-        : 'Total de usuários: ${usuarios.lenght}',
+        : `Total de usuários: ${usuarios.length}`,
         usuarios,
     });
 });
@@ -27,5 +27,29 @@ usuariosRoutes.post("/", (req, res) => {
         usuario,
     });
 });
+
+
+usuariosRoutes.get("/:id", (req, res) => {
+    const {id} = req.params;
+    const user = usersRepository.getUserById(id);
+
+    if(!user) {
+        return res.status(404).json({
+            message: `Usuário com id ${id} não encontrado!`,
+        });
+    }
+
+    return res.status(200).json ({
+        message: `Usuário com o id ${id} encontrado!`,
+        user,
+    });
+});
+
+usuariosRoutes.get("/:id", (req, res) => {
+
+})
+usuariosRoutes.get("/:id", (req, res) => {
+
+})
 
 export default usuariosRoutes;
