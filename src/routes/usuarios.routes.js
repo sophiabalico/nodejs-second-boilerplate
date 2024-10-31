4675ffda7dd841d49d6e5a0ff030f17a8f923fd8
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-10-31 11:29:15 -0300

    feat: create delete users method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -65,7 +65,20 @@ usuariosRoutes.put("/:id", (req, res) => {
 });
 
 usuariosRoutes.delete("/:id", (req, res) => {
+    const { id } = req.params;
 
-})
+    const user = usersRepository.deleteUser(id)
+
+    if(!user) {
+        return res.status(404).json({
+            message: `Usuário com id ${id} não encontrado!`,
+        });
+    }
+
+    return res.status(200).json ({
+        message: `Usuário com o id ${id} deletado com sucesso!`,
+        user,
+    });
+});
 
 export default usuariosRoutes;
